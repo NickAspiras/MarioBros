65e2d3a6af1a30068e9e6d0cb712565dc74d5630
NickAspiras <57922966+NickAspiras@users.noreply.github.com>
2021-04-22 16:36:34 -0700

    Add files via upload

---
+++ core/src/com/nickaspiras/mariobros/Tools/B2WorldCreator.java
@@ -0,0 +1,88 @@
+package com.nickaspiras.mariobros.Tools;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
+import com.nickaspiras.mariobros.MarioBros;
+import com.nickaspiras.mariobros.Screens.PlayScreen;
+import com.nickaspiras.mariobros.Sprites.*;
+
+public class B2WorldCreator {
+    private Array<Goomba> goombas;
+    private static Array<Turtle> turtles;
+
+    public B2WorldCreator(PlayScreen screen) {
+
+        World world = screen.getWorld();
+        TiledMap map = screen.getMap();
+        //create body and fixture variables
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        //ground
+
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2)
+                    / MarioBros.PPM, (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2 / MarioBros.PPM, rect.getHeight() / 2 / MarioBros.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        //pipes
+        for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2)
+                    / MarioBros.PPM, (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2 / MarioBros.PPM, rect.getHeight() / 2 / MarioBros.PPM);
+            fdef.shape = shape;
+            fdef.filter.categoryBits = MarioBros.OBJECT_BIT;
+            body.createFixture(fdef);
+        }
+
+        //bricks
+        for (MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+            new Brick(screen, object);
+        }
+
+        //coins
+        for (MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            new Coin(screen, object);
+        }
+
+        //goombas
+        goombas = new Array<Goomba>();
+        for (MapObject object : map.getLayers().get(6).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            goombas.add(new Goomba(screen, rect.getX() / MarioBros.PPM, rect.getY() / MarioBros.PPM));
+        }
+        turtles = new Array<Turtle>();
+        for (MapObject object : map.getLayers().get(7).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            turtles.add(new Turtle(screen, rect.getX() / MarioBros.PPM, rect.getY() / MarioBros.PPM));
+        }
+    }
+    public static void removeTurtle(Turtle turtle){
+        turtles.removeValue(turtle, true);
+    }
+    public Array<Enemy> getEnemies() {
+        Array<Enemy> enemies = new Array<Enemy>();
+        enemies.addAll(goombas);
+        enemies.addAll(turtles);
+        return enemies;
+    }
+
+
+}
